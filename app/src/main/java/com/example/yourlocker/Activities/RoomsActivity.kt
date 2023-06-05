bd4feb81081c7d8a14a390b46b65f24428947d45
BustosNahu <nbustosalexander@gmail>
2023-06-05 09:09:44 -0300

    have done: -Add new activity in kotlin with jetpack compose (RoomsActivity)
    still needed:
    -Make UI and add new fragment screen to add new devices, and finish lazyRow

---
+++ app/src/main/java/com/example/yourlocker/Activities/RoomsActivity.kt
@@ -0,0 +1,47 @@
+package com.example.yourlocker.Activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.yourlocker.Activities.ui.theme.YourLockerTheme
+
+class RoomsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            YourLockerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello como estas $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true,
+    showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    YourLockerTheme {
+        Greeting("Android")
+    }
+}
