5690d3e2a30b66e877cb637e656a0eef11568474
BustosNahu <nbustosalexander@gmail>
2023-09-20 18:50:28 -0300

    Login finished

--- app/src/main/java/com/example/yourlocker/RoomUi/AddDevicesScreen.kt
+++ app/src/main/java/com/example/yourlocker/RoomUi/AddDevicesScreen.kt
@@ -1,7 +1,10 @@
 package com.example.yourlocker.RoomUi
 
+import android.content.ClipData.Item
+import android.graphics.Paint.Style
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +14,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
@@ -24,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -213,3 +220,4 @@ fun addNewRoom(roomId: String) {
 fun AddDevicesScreenPreview() {
     AddDevicesScreen(navController = rememberNavController(), "")
 }
+
